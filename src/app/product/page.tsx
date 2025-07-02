a064d5ef80ba95c0ff778844ca3257d4172d815d
OmarAguilar43 <omaroaguilar43@gmail.com>
2025-07-01 22:49:12 -0600

    slider

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { ProductSlide } from "@/src/components/galery/productSlideShow"
+
 
 
 
@@ -11,17 +13,29 @@ export default  function productPage() {
     <div className="mt-15 mb-20 grid grid-cols-1  md:grid-cols-3 gap-3">
 
       {/* {Slideshow} */}
-      <div className="col-span-1 md:col-span-2 bg-red-500">
-        <h1>Hola mundo</h1>
+      <div className="col-span-1 md:col-span-2 ">
+        <ProductSlide/>
       </div>
 
       {/* {detalles} */}
 
-      <div className="col-span-1 px-5 bg-blue-500">
+      <div className="col-span-1 px-5 ">
         <h1 className="font-bold text-2xl antialiased">Ola 2</h1>
-        <p className="">$75</p>
-      </div>
+        <p className="text-lg mb-5">$75</p>
+      
 
+      {/* {boton} */}
+
+      <button className="my-5 bg-blue-600 
+        hover:bg-blue-800 text-white py-2 px-4 rounded transition-all">
+        Ordenar
+      </button>
+
+      {/* {descripcion} */}
+      <h3 className="font-bold text-sm">Description</h3>
+      <p className="font-light">mucho texto </p>
+
+    </div>
     </div>
 
   )
---
+++ src/components/galery/productSlideShow.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import { Swiper as SwiperType } from 'swiper'
+import { useState } from 'react'
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
+import Image from 'next/image'
+
+export const ProductSlide = () => {
+  const images = ['/shirtNew.png', '/shirt_back.PNG', '/body.png']
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null)
+
+  return (
+    <>
+      {/* Carrusel principal */}
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
+        } as React.CSSProperties}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="w-full md:w-[500px] h-[400px] md:h-[550px]"
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <div className="relative w-full h-[400px] md:h-[550px]">
+              <Image
+                src={src}
+                alt={`Imagen ${index + 1}`}
+                fill
+                className="object-contain rounded-lg"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Carrusel de miniaturas */}
+      <div className="mt-2 w-full md:w-[600px] px-4">
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={7}
+          slidesPerView={3}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="h-[110px]"
+        >
+          {images.map((src, index) => (
+            <SwiperSlide key={`thumb-${index}`} className="cursor-pointer">
+              <div className="relative w-[100px] h-[100px] border border-white/20 hover:border-white transition">
+                <Image
+                  src={src}
+                  alt={`Miniatura ${index + 1}`}
+                  fill
+                  className="object-contain rounded-md"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  )
+}
