858519482003619a72187ba9b5c7481ae5979316
OmarAguilar43 <omaroaguilar43@gmail.com>
2025-07-02 22:40:35 -0600

    Mis cambios locales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 'use client'
 
-import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
 
@@ -10,21 +9,31 @@ import { useRouter } from 'next/navigation'
 export default function SplashPage() {
   const router = useRouter()
 
-  const handleClick = () => {
+  const handleClickShop = () => {
     router.push('/shop')
   }
 
+  const handleClickAboutUs = () => {
+    router.push('/nosotros')
+  }
+
   return (
-    <div
-      onClick={handleClick} className="flex items-center justify-center h-screen w-full bg-black cursor-pointer"
-    >
-      <Image
-        src="/logo.PNG" // o logo.png si es PNG
-        alt="Dalloway Logo"
-        width={400}
-        height={400}
-        className="hover:scale-110 transition-transform"
-      />
+    <div className="flex flex-row justify-start items-end h-screen w-full sm:bg-[url(/shirt_back.png)] bg-[url(/body.png)] bg-no-repeat bg-cover bg-center">
+      <div className="flex flex-col justify-center items-center sm:items-end h-full w-full 2xl:w-40/100 xl:w-50/100 lg:w-60/100 sm:w-65/100 bg-red-500lolo">
+        <img
+          src="/shirtNew.PNG" // o logo.png si es PNG
+          alt="Dalloway Logo"
+          className="w-85/100 bg-blue-500lolo"
+        />
+        <div className="flex flex-row gap-x-15 justify-center items-center w-85/100 mt-5 bg-blacklolo">
+          <button className="bg-green-500lolo">
+            Comprar
+          </button>
+          <button className="bg-green-500lolo">
+            Nosotros
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
