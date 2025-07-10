e2621182005d050d99780120a485c4f0913d393a
Brandom Galder Hernández Franco <galderhernandez571@gmail.com>
2025-07-10 14:00:21 -0600

    landingpage finalized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,19 +18,19 @@ export default function SplashPage() {
   }
 
   return (
-    <div className="flex flex-row justify-start items-end h-screen w-screen bg-black bg-no-repeat bg-cover bg-center">
-      {/*añadir sm:bg-[url(/shirt_back.png)] bg-[url(/body.png)] arriba*/}
-      <div className="flex flex-col justify-center items-center sm:items-end h-full w-full 2xl:w-40/100 xl:w-50/100 lg:w-60/100 sm:w-65/100 bg-red-500lolo">
+    <div className="flex flex-row justify-start items-end h-screen w-screen sm:bg-[url(/banner-escritorio.jpg)]  bg-[url(/banner-movil.jpg)] bg-no-repeat bg-cover bg-center">
+      {/*añadir  arriba*/}
+      <div className="flex flex-col justify-center items-center h-full w-full sm:w-60/100 bg-red-500lolo">
         <img
             src="/logo-plata.png" // o logo.png si es PNG
             alt="Dalloway Logo"
-            className="w-85/100 bg-green-500lolo"
+            className="h-50/100 sm:h-70/100 bg-green-500lolo"
           />
-        <div className="flex flex-row gap-x-5 justify-center items-center w-85/100 mt-5 bg-blacklolo">
-          <button  onClick={handleClickProduct} className="border-2 w-50/100  p-2 text-xs hover:bg-slate-900 bg-green-500lolo">
+        <div className="flex flex-row gap-x-5 justify-center items-center w-55/100 mt-5 bg-blacklolo">
+          <button  onClick={handleClickProduct} className="border-2 w-50/100  p-2 lg:text-base text-xs hover:bg-slate-900 bg-green-500lolo">
             COMPRAR
           </button>
-          <button onClick={handleClickAboutUs} className="border-2 w-50/100  p-2 text-xs hover:bg-slate-900 bg-green-500lolo">
+          <button onClick={handleClickAboutUs} className="border-2 w-50/100  p-2 lg:text-base text-xs hover:bg-slate-900 bg-green-500lolo">
             NOSOTROS
           </button>
         </div>
