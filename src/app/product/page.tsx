121ab0bc351b75eb1e54495ed04c72df8e202c67
Brandom Galder Hernández Franco <galderhernandez571@gmail.com>
2025-07-10 14:10:52 -0600

    fixing navbar component with production page integration

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,102 +1,94 @@
-'use client'
+"use client";
 
 //import { ProductSlide } from "@/src/components/galery/productSlideShow"
-import { ButtonSlideTallas } from '../../components/buttons/buttonSlideTallas';
+import { ButtonSlideTallas } from "../../components/buttons/buttonSlideTallas";
 import { ButtonSlideDetalles } from "@/src/components/buttons/buttonSlideDetalles";
 import { Navbar } from "@/src/components/Navbar";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import EmblaCarousel from "@/src/components/newGalery/EmblaCarousel";
 
-
-
-
-
-
-export default  function productPage() {
-
-  const slides = ['/KANZ V2 ESPALDA.jpg','/KANZ V2 FRENTE.jpg','/1.jpg', '/2.jpg', '/3.jpg', '/4.jpg', '/5.jpg'];
-  
+export default function productPage() {
+  const slides = [
+    "/KANZ V2 ESPALDA.jpg",
+    "/KANZ V2 FRENTE.jpg",
+    "/1.jpg",
+    "/2.jpg",
+    "/3.jpg",
+    "/4.jpg",
+    "/5.jpg",
+  ];
 
   const router = useRouter();
 
   const handleClick = () => {
     router.push("/nosotros");
-  }
+  };
 
   return (
     <div className="w-full h-full bg-black text-white">
-      <Navbar 
-        texto = "NOSOTROS"
-        fn = {handleClick}
-      />
+      <Navbar texto="NOSOTROS" fn={handleClick} />
 
-      
-    <div className="mt-2 mb-20 grid grid-cols-1 bg-black md:grid-cols-2 gap-2">
-      {/* {Slideshow} */}
-      <div className="col-span-1 w-full max-w-[800px] md:col-span-1 ">
-   
-         <EmblaCarousel slides={slides} />
-        
-      </div>
+      <div className="w-full h-full">
+        <div className="mt-2 mb-20 grid grid-cols-1 bg-black md:grid-cols-2 gap-2">
+          {/* {Slideshow} */}
+          <div className="col-span-1 w-full max-w-[800px] md:col-span-1 ">
+            <EmblaCarousel slides={slides} />
+          </div>
 
-      {/* {detalles} */}
+          {/* {detalles} */}
 
-      
+          {/* <div className="col-span-1 px-10 mt-20 items-center sm:mt-5 md:mt-5"> */}
 
-      {/* <div className="col-span-1 px-10 mt-20 items-center sm:mt-5 md:mt-5"> */}
+          <div className="col-span-1 px-10 flex items-center justify-center max-w-[500px] mx-auto">
+            <div className="flex flex-col justify-center">
+              {/* TODO: tu contenido aquí */}
 
-      <div className="col-span-1 px-10 flex items-center justify-center max-w-[500px] mx-auto">
+              <h1
+                style={{ fontFamily: "Panchang-Bold" }}
+                className="font-bold text-4xl antialiased text-white"
+              >
+                <span className="block">KANZ KROPPED</span>
+                <span className="block">T-SHIRT V2</span>
+              </h1>
 
-        <div className="flex flex-col justify-center">
-    {/* TODO: tu contenido aquí */}
-            
+              <p
+                style={{ fontFamily: "Panchang-Regular" }}
+                className="text-sm mb-5 mt-10"
+              >
+                $ 799 MXN
+              </p>
 
-        <h1
-          style={{ fontFamily: 'Panchang-Bold' }}
-          className="font-bold text-4xl antialiased text-white"
-        >
-          <span className="block">KANZ KROPPED</span>
-          <span className="block">T-SHIRT V2</span>
-        </h1>
+              {/* {boton} */}
 
-        <p style={{ fontFamily: 'Panchang-Regular' }}className="text-sm mb-5 mt-10">$ 799 MXN</p>
-      
+              <button
+                style={{ fontFamily: "Panchang-Medium" }}
+                className="my-5 
+        hover:bg-slate-900 text-white py-4 w-full rounded border-3 border-white cursor-pointer transition delay-50 duration-300 ease-in-out"
+              >
+                Ordenar
+              </button>
 
-         {/* {boton} */}
+              {/* {second buttons} */}
 
-        <button style={{ fontFamily: 'Panchang-Medium' }} className="my-5 
-        hover:bg-slate-900 text-white py-4 w-full rounded border-3 border-white cursor-pointer transition delay-50 duration-300 ease-in-out">
-        Ordenar
-        </button>
-      
-        {/* {second buttons} */}
-
-        <div className="grid grid-cols-2 gap-4 ">
-
-          {/* <button style={{ fontFamily: 'Panchang-Bold' }} 
+              <div className="grid grid-cols-2 gap-4 ">
+                {/* <button style={{ fontFamily: 'Panchang-Bold' }} 
                   className="border-2 py-2 text-xs hover:bg-slate-900">DETALLES</button>
 
           <button style={{ fontFamily: 'Panchang-Bold' }}
                   className="border-2 text-xs hover:bg-slate-900">TALLAS</button> */}
 
-          <ButtonSlideTallas />
+                <ButtonSlideTallas />
 
-          <ButtonSlideDetalles/>
+                <ButtonSlideDetalles />
+              </div>
 
+              {/* {descripcion} */}
+              <h3 className="font-bold text-sm">Description</h3>
+              <p className="font-light">mucho texto </p>
+            </div>
+          </div>
         </div>
-
-       {/* {descripcion} */}
-        <h3 className="font-bold text-sm">Description</h3>
-        <p className="font-light">mucho texto </p>
-
       </div>
     </div>
-
-
-          </div>
-    </div>
-
-    
-
-  )
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ type navBarProps = {
 
 export function Navbar({ texto, fn }: navBarProps) {
   return (
-    <header className="sticky top-0 left-0 w-full h-20 py-3 box-content z-1 bg-transparent text-white">
+    <header className="sticky top-0 left-0 w-full h-20 py-3 box-content z-1 bg-black text-white">
       <nav className="flex w-full h-full justify-center items-center bg-blue-500lolo">
         <div className="w-15/100 h-full hidden lg:block bg-orange-500lolo"></div>
         <div className="flex justify-start lg:justify-center w-70/100 max-md:w-35/100 h-full py-2 max-lg:pl-10 bg-green-500lolo">
