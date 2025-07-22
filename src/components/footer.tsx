8ef2f424c82610c87e219e2d5eace31a95c864a2
Brandom Galder Hernández Franco <galderhernandez571@gmail.com>
2025-07-22 15:56:35 -0600

    feat: update footer component with new layout and add social media links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,27 +1,22 @@
-
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export const Footer = () => {
   return (
-    <div className='flex w-full justify-center text-xs mb-10'>
-        <Link href='/'>
-            <span className={'antialiased font-bold'}>KANZ</span>
-            <span>| shop</span>
-            <span>{new Date().getFullYear()}</span>
-        </Link>
-
-      <Link
-      href='/'
-      className='mx-3'>
-        Privacidad y legal
+    <div className="flex w-full h-8 justify-center gap-x-7 items-center text-xs md:mb-10 mb-15">
+      <Link href="/" className="w-fit">
+        <span className="antialiased font-bold mr-1">KANZ</span>
+        <span>| shop{new Date().getFullYear()}</span>
       </Link>
 
-      <Link
-      href='/'
-      className='mx-3'>
-        Ubicaciones
-      </Link>
+      <div className="flex justify-between h-75/100 gap-x-5">
+        <a href="https://www.instagram.com/kanz2wear?igsh=ZjZhdThhdjdkN3Fs" className="h-full">
+          <img src="/logos/instagram-logo.svg" alt="Instagram Logo" className="h-full" />
+        </a>
+        <a href="https://www.tiktok.com/@kanz_2_wear1?_t=ZS-8yFZD5aOO8Y&_r=1" className="h-full">
+          <img src="/logos/tiktok-logo.svg" alt="TikTok Logo" className="h-full" />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
